6fe58eddd9e56c4d1bd4a32f80616eb4ede93075
JSaturn9 <JSaturn9@github.com>
2020-09-21 18:41:52 -0400

    Added relevant icon from API

--- src/app.js
+++ src/app.js
@@ -22,23 +22,26 @@ let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
 }
 
-function displayTemperature (response) {  
+function displayTemperature (response) { 
  let temperatureElement = document.querySelector("#temperature");
  let cityElement = document.querySelector("#city");
  let descriptionElement = document.querySelector("#description");
  let humidityElement = document.querySelector("#humidity");
  let windElement = document.querySelector("#wind");
  let dateElement = document.querySelector("#date");
+ let iconElement = document.querySelector("#icon");
  temperatureElement.innerHTML = Math.round(response.data.main.temp);
  cityElement.innerHTML = response.data.name;
  descriptionElement.innerHTML = response.data.weather[0].description;
  humidityElement.innerHTML = response.data.main.humidity;
  windElement.innerHTML = Math.round(response.data.wind.speed);
  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+ iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+ iconElement.setAttribute("alt", response.data.weather[0].icon);
 }
 
 let apiKey = "8aeae4561f5b9cd8fde3dc22aa02472c";
-let city = "Paris";
+let city = "New York";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
 
